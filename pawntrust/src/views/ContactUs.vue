bee3cfac2219221a9e046d39c259942746595e84
Madan Kumar <madan@kbxdigital.com>
2022-12-03 13:49:39 +0530

    contact us

--- pawntrust/src/views/ContactUs.vue
+++ pawntrust/src/views/ContactUs.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <TopBar></TopBar>
-        <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2071.331691630778!2d-84.3862489769204!3d33.77469388925303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5046f5730dc11%3A0x1093833a7eb0ce38!2s730%20Midtown!5e0!3m2!1sen!2sin!4v1669055037440!5m2!1sen!2sin" width="1200" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div v-if="$mq == 'desktop'">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2071.331691630778!2d-84.3862489769204!3d33.77469388925303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5046f5730dc11%3A0x1093833a7eb0ce38!2s730%20Midtown!5e0!3m2!1sen!2sin!4v1669055037440!5m2!1sen!2sin" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div v-else-if="$mq == 'tablet'">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3316.464362533766!2d-84.38705298481712!3d33.77450478068235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5046f5730dc11%3A0x1093833a7eb0ce38!2s730%20Midtown!5e0!3m2!1sen!2sin!4v1670053402414!5m2!1sen!2sin" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div v-else>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3316.464362533766!2d-84.38705298481712!3d33.77450478068235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5046f5730dc11%3A0x1093833a7eb0ce38!2s730%20Midtown!5e0!3m2!1sen!2sin!4v1670053402414!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="mainClass">
             <span style="font-size:2rem;font-weight:600; color:#FFFFFF;">Contact Us</span>
@@ -226,7 +232,7 @@ import emailjs from 'emailjs-com';
        width:95%;
     }
     .mainClass{
-        background-color:#F19B14;height: 75vh;
+        background-color:#F19B14;
     }
 
     
@@ -243,7 +249,7 @@ import emailjs from 'emailjs-com';
        width:76%;
     }
     .mainClass{
-        background-color:#F19B14;height: 80vh;
+        background-color:#F19B14;
     }
 }
 
