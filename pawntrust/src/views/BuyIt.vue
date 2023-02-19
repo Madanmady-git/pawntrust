1383d6da923b1b64bbbfc2425d18042a67db3cb9
Madan Kumar <madan@kbxdigital.com>
2023-02-20 01:49:19 +0530

    changes

--- pawntrust/src/views/BuyIt.vue
+++ pawntrust/src/views/BuyIt.vue
@@ -1,34 +1,54 @@
 <template>
     <div >
         <TopBar></TopBar>
-        <div style="display: flex;flex-direction: row;">
-            <v-navigation-drawer
-                    >
-                    <v-list>
-                        <v-btn 
-                        @click="SellItAction()"
-                        style="display: flex;width: 100%;box-shadow: none;text-transform: capitalize;color: #FFF;background-color: #F19B14;">
-                            Sell It
-                        </v-btn>
-                        <v-list-item
-                        v-for="item in items"
-                        :key="item.title"
-                        link
-                        active-color="primary"
-                        style="display: flex;justify-content: flex-start;"
+        <v-divider style="width:100%;"></v-divider>
+        <div class="mainContainer">
+            <div class="navBar">
+                <v-navigation-drawer
                         >
-                        <v-list-item-icon>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-item-icon>
-                        <span>{{ item.title }}</span>
-                        <!-- <v-list-item-content>
-                            <v-list-item-title>{{ item.title }}</v-list-item-title>
-                        </v-list-item-content> -->
-                        </v-list-item>
-                    </v-list>
-            </v-navigation-drawer>
+                        <v-list>
+                            <v-btn 
+                            @click="SellItAction()"
+                            style="display: flex;width: 100%;box-shadow: none;text-transform: capitalize;color: #FFF;background-color: #F19B14;">
+                                Sell It
+                            </v-btn>
+                            <v-list-item
+                            v-for="item in items"
+                            :key="item.title"
+                            link
+                            active-color="primary"
+                            style="display: flex;justify-content: flex-start;"
+                            >
+                            <v-list-item-icon>
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-item-icon>
+                            <span>{{ item.title }}</span>
+                            <!-- <v-list-item-content>
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                            </v-list-item-content> -->
+                            </v-list-item>
+                        </v-list>
+                </v-navigation-drawer>
+            </div>
+            <div class="TabBar">
+                <v-tabs
+                    v-model="tab"
+                    active-class="TabClass"
+                    hide-slider
+                    >
+
+                    <v-tab
+                        v-for="item in items2"
+                        :key="item"
+                        style="text-transform:capitalize;font-size: 1rem;font-weight: 600;"
+                    >
+                        {{ item }}
+                    </v-tab>
+                </v-tabs>
+                <v-divider></v-divider>
+            </div>
             <div class="mainDiv">
-                <div class="cardContent">
+                <div class="cardContent mobileCardContent">
                     <span style="font-size:2.3rem;font-weight:600;">Buy It</span>
                 </div>
                 <!-- <div class="cardContentHeader">
@@ -60,10 +80,10 @@
                         </div>
                     </div>
                 </div> -->
-                <div class="cardContent">
+                <div class="cardContent mobileCardContent">
                     <span style="font-size:1.3rem;font-weight:600;padding: 2% 0%;">Products</span>
                 </div>
-                <div class="cardContent">
+                <div class="cardContent3">
                     <div class="cardContentHorizontal">
                         <div v-for="i in 13" :key="i">
                             <ProductCard/>
@@ -89,6 +109,12 @@ import ProductCard from './ProductCard.vue';
                     { title: 'Category', icon: 'mdi-view-dashboard' },
                     { title: 'Price', icon: 'mdi-view-dashboard' },
                 ],
+                tab: null,
+                items2: [
+                'Sell It', 'Categories', 'Price'
+                ],
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            
              }
             },
         methods:{
@@ -105,18 +131,61 @@ import ProductCard from './ProductCard.vue';
 <style scoped>
 
 @media screen and (max-width:900px) {
+    .mainContainer{
+        /* display: flex; */
+        /* flex-direction: column; */
+    }
+    .mobileCardContent{
+        padding-left: 3%;
+    }
     .cardContent{
-        width: 80%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    .cardContent3{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
     .mainDiv{
-        width: 80%;
+        width: 100%;
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+    .navBar{
+        display: none;
+    }
+    .TabBar{
+
+    }
+    .TabClass{
+        background-color: #F19B14;
+        color: #FFF;
+        border-radius: 12px;
+    }
+    .cardContentHorizontal{
+        width: 100%;
+        display: flex;
+        /* justify-content: space-between; */
+        flex-wrap: wrap;
+        gap:2%;
+        column-gap: 2%;
+        align-items: center;
     }
 }
 
 @media screen and (min-width:901px) {
-    .cardContent{
+    .mainContainer{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
+    .cardContent, .cardContent3{
         width: 90%;
         display: flex;
         flex-direction: column;
@@ -128,6 +197,9 @@ import ProductCard from './ProductCard.vue';
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+    }
+    .TabBar{
+        display: none;
     }
     .cardContentHorizontal{
         width: 100%;
--- pawntrust/src/views/ProductDetails.vue
+++ pawntrust/src/views/ProductDetails.vue
@@ -1,8 +1,8 @@
 <template>
     <div >
         <TopBar></TopBar>
-        <div style="display:flex; flex-direction: row; width:100%;">
-            <div style="display: flex; width:70%;">
+        <div class="mainContainer">
+            <div class="corosalCard">
                 <v-carousel
                 cycle
                 height="600"
@@ -17,7 +17,7 @@
                 </v-carousel>
             </div>
             
-            <div style="display:flex;flex-direction: column;align-items: flex-start;width: 30%;padding:5%;">
+            <div class="InformationCard">
                 <v-card style="display:flex;flex-direction: column;align-items: flex-start;padding:5%;margin-bottom: 4%;">
                     <!-- <v-divider vertical style="margin-top:10px;"></v-divider> -->
                     <span class="fontStyleBold">$ 400</span>
@@ -25,7 +25,7 @@
                     <span style="text-align: left;">Exclusive Swiss Made Luxury watch and very rare model in this brand Exclusive Swiss Made Luxury watch and very rare model in this brand Exclusive Swiss Made Luxury watch and very rare model in this brand Exclusive Swiss Made Luxury watch and very rare model in this brand</span>
                     <span class="fontStyleSmaller">Bought 2 months before</span>
                 </v-card>
-                <v-card @click="click = !click" style="width:100%;margin-bottom:4%">
+                <v-card @click="click = !click" class="InterestedWidth">
                     <div  :class="click ? 'Notified' : 'Interested'">{{ click ? 'Notified' : 'I am interested' }}</div>
                 </v-card>
                 <v-card style="width:100%;margin-bottom:4%; padding:4%;">
@@ -82,10 +82,90 @@ import Footer from '../components/Footer.vue';
 <style scoped>
 
 @media screen and (max-width:900px) {
-
+    .mainContainer{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        width:100%;
+    }
+    .corosalCard{
+        display: flex; width:90%;
+    }
+    .InformationCard{
+        display:flex;flex-direction: column;align-items: flex-start;width: 90%;
+    }
+    .InterestedWidth{
+        width:100%;margin-bottom:4%
+    }
+    .ProductCard {
+        width: 240px;
+        padding: 3%;
+        display: flex;
+        flex-direction: column;
+        margin-bottom :10%;
+    }
+    .alignLeft{
+        display: flex;
+        justify-content: flex-start;
+        padding: 2% 6%;
+    }
+    .fontStyleBold{
+        font-size: 1.6rem;
+        font-weight: bold;
+    }
+    .fontStyleLightBold{
+        font-size: 1rem;
+        font-weight: 550;
+    }
+    .fontStyleSmaller{
+        font-size: 0.8rem;
+        font-weight: 550;
+    }
+    .Interested{
+        width: 100%;
+        padding: 3%;
+        border: 2px black solid;
+        border-radius: 8px;
+        font-size: 1.2rem;
+        font-weight: 600;
+    }
+    .Notified{
+        width: 100%;
+        padding: 3%;
+        border: 2px white solid;
+        border-radius: 8px;
+        font-size: 1.2rem;
+        font-weight: 600;
+        background-color: #F19B14;
+        color:#FFFFFF;
+    }
+    .Notified-2{
+        width: 100%;
+        padding: 3%;
+        border: 2px #f2f2f2 solid;
+        border-radius: 8px;
+        font-size: 1rem;
+        font-weight: 600;
+        background-color: #F19B14;
+        color:#FFFFFF;
+    }
 }
 
 @media screen and (min-width:901px) {
+    .mainContainer{
+        display:flex;
+        flex-direction: row;
+        width:100%;
+    }
+    .corosalCard{
+        display: flex; width:70%;
+    }
+    .InformationCard{
+        display:flex;flex-direction: column;align-items: flex-start;width: 30%;padding:5%
+    }
+    .InterestedWidth{
+        width:100%;margin-bottom:4%
+    }
     .ProductCard {
         width: 240px;
         padding: 3%;
