080cd32b5a540a2e3d9feea6d447700028cfcca5
Madan Kumar <madan@kbxdigital.com>
2023-06-23 09:29:17 +0530

    changes

--- pawntrust/src/components/Headings.vue
+++ pawntrust/src/components/Headings.vue
@@ -1,13 +1,27 @@
-    <template>
+<template>
     <div class="text-center" style="padding: 12px 0% 0% 0%;" :class="$mq != 'desktop' ? 'mainClassMobile' : ''">
         <v-menu
         transition="scroll-y-transition"
-        open-on-click
-        bottom
-        offset-y
         open-on-hover
         :min-width="$mq != 'desktop' ? '100%' : ''"
         >
+        <template v-slot:activator="{ on, attrs }">
+            <v-hover v-slot="{ hover }" >
+                <v-btn
+                text
+                v-bind="attrs"
+                v-on="on"
+                @click="routeToRightMethod(headingName)"
+                style="text-transform: capitalize;font-size: 17px;font-weight: 600;letter-spacing: 0rem;"
+                :style="{ 'background-color': hover ? '#F19B14' : '#FFFFFF'}"
+                >
+                <div v-if="headingName == 'More'" align-items = "right">
+                    <v-app-bar-nav-icon color="#000000"></v-app-bar-nav-icon>
+                </div>
+                <span v-else>{{headingName}}</span>
+                </v-btn>
+            </v-hover>
+        </template>
         <v-card :class="$mq != 'desktop' ? 'menuClass' : ''">
             <v-list v-if="items">
                 <v-list-item
@@ -45,7 +59,6 @@
         </div>
     </div>
 </template>
-
 <script>
 import login from '../views/LoginPopUp.vue'
 export default {
@@ -146,12 +159,10 @@ export default {
     }
 }
 </script>
-
 <style scoped>
 .menuClass{
     width:100%;
 }
-
 .mainClassMobile{
     display: flex;
     justify-content: flex-end;
