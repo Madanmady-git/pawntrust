f264e9c6fbcc701d450a668307a10fd612c201e5
Madan Kumar <madan@kbxdigital.com>
2023-08-08 22:50:55 +0530

    changes for brochure api

---
+++ pawntrust/src/components/TopBarDummy.vue
@@ -0,0 +1,293 @@
+<template>
+    <div>
+        <div :class="($mq =='mobile' || $mq == 'tablet') ? 'mainMobileClass' : 'mainDesktopClass'">
+            <div v-if="$mq == 'desktop'" class="DesktopLogo">
+                <img :class="($mq =='mobile') ? 'logoImageMobile' :  ''"
+                    src="../assets/header-logo.png"
+                />
+            </div>
+            <div v-if="$mq == 'desktop'" :class="($mq =='mobile' || $mq == 'tablet') ? 'headerAlignMobile2' : 'headerAlignDesktop'">
+                <v-btn icon style="cursor: pointer;" href="https://www.facebook.com/pawntrust" target="_blank"><v-icon color="#F19B14" size="40">mdi-facebook</v-icon></v-btn>
+                <v-btn icon style="cursor: pointer;" href="https://twitter.com/dinewiseinc" target="_blank"><v-icon color="#F19B14" size="40">mdi-twitter</v-icon></v-btn>
+                <v-btn icon style="cursor: pointer;" href="https://www.instagram.com/dinewiseinc/" target="_blank"><v-icon color="#F19B14" size="40">mdi-instagram</v-icon></v-btn>
+            </div>
+            <div :class="($mq =='mobile' || $mq == 'tablet') ? 'mobileTopBarContent' : 'DesktopTopBarContent'">
+                <div :class="$mq != 'desktop' ? 'mobileMenuClass' : ''">
+                    <div style="width:40%;">
+                        <div v-if="$mq != 'desktop'" class="mobileLogo">
+                            <img style="width:100%;"
+                                src="../assets/header-logo.png"
+                            />
+                        </div>
+                    </div>
+                    <div :class="($mq =='mobile' || $mq == 'tablet') ? 'headerAlignMobile2' : 'headerNoAlignDesktop'">
+                        <v-btn icon style="cursor: pointer;" href="https://www.facebook.com/pawntrust" target="_blank"><v-icon color="#F19B14" size="40">mdi-facebook</v-icon></v-btn>
+                        <v-btn icon style="cursor: pointer;" href="https://twitter.com/dinewiseinc" target="_blank"><v-icon color="#F19B14" size="40">mdi-twitter</v-icon></v-btn>
+                        <v-btn icon style="cursor: pointer;" href="https://www.instagram.com/dinewiseinc/" target="_blank"><v-icon color="#F19B14" size="40">mdi-instagram</v-icon></v-btn>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Heading from '../components/Headings.vue';
+export default {
+        components: { Heading },
+        props:['page'],
+        data(){
+            return{
+                token : '',
+                search : '',
+                showSignInSignUp: true,
+                name: 'Yeshwanth',
+                featureItems : [
+                    // { title: 'Pawn Shops' },
+                    { title: 'Titlepal' },
+                    // { title: 'Microlending' },
+                    // { title: 'Non-Fungible Tokens' },
+                ],
+                moreItems : [
+                    {title: 'Contact Us'},
+                    { title: 'About Us' },
+                    { title: 'Investor Relations' },
+                ],
+                allItems :[
+                    { title: 'Home'},
+                    { title: 'Pawn Scan' },
+                    // { title: 'Pawn It' },
+                    { title: 'Sell It' },
+                    // { title: 'Buy It' },
+                    // { title: 'Pawn Shops' },
+                    { title: 'Titlepal' },
+                    // { title: 'Microlending' },
+                    // { title: 'Non-Fungible Tokens' },
+                    { title: 'Contact Us'},
+                    { title: 'About Us' },
+                    { title: 'Investor Relations' },
+                    
+                ]
+            }
+        },
+        mounted(){
+            this.token = this.$cookies.get('token');
+            this.name = this.$cookies.get('name');
+            if (this.token) {
+                this.showSignInSignUp = false;
+            } else {
+                this.showSignInSignUp = true;
+            }
+        },
+        methods: {
+            searchMethod(word){
+                console.log("Entered in search Area");
+                this.$router.push({
+                    name:'Pawn Scan',
+                    params : {
+                        searchword : word
+                    }
+                }).catch(error => {
+                    console.log(error)
+                })
+            },
+            SignIn(){
+                this.$router.push({
+                    name:'Login'
+                }).catch(error => {
+                    console.log(error)
+                })
+            },
+            logOut(){
+                this.$cookies.remove('token');
+                this.$router.push({
+                    name : 'Login'
+                }).catch(error => {
+                    console.log(error)
+                })
+            },
+            SignUp(){
+                this.$router.push({
+                    name:'SignUp'
+                }).catch(error => {
+                    console.log(error)
+                })
+            },
+            clickedProfile(){
+                this.$router.push({
+                    name : 'Profile'
+                })
+                .catch((error)=>{
+                    console.log('error', error)
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.mainMobileClass {
+    display:flex; flex-direction : row;justify-content:flex-end; width: 100%;padding: 8px;
+}
+.mainDesktopClass {
+    display:flex; flex-direction : row;justify-content:center; width: 100%;padding: 8px;
+}
+
+.topBarMobile{
+    display:flex; flex-direction : column;align-items: flex-end; padding:1%;
+}
+
+.topBarDesktop{
+    display:flex; flex-direction : row;justify-content:space-around;padding:14px
+}
+
+.headerAlignMobile{
+    display:flex; flex-direction : row;justify-content:flex-end;padding: 1% 0%;
+}
+.headerAlignMobile2{
+    width:50%; display:flex; flex-direction : row;justify-content:space-between; padding: 1% 0%;
+}
+
+.headerAlignMobile3{
+    width:60%; display:flex; flex-direction : row;justify-content:space-between; padding: 1% 0%;
+}
+
+.headerAlignDesktop{
+    display:flex; flex-direction : row;justify-content:space-around;align-items: center; width:100%;
+}
+.headerNoAlignDesktop{
+    display: none;
+}
+.mobileLogo{
+    width: 100%;display: flex;flex-direction: row; justify-content: center;align-items:center;
+}
+
+.DesktopLogo{
+    width: 100%;display: flex;flex-direction: row; justify-content: center;align-items:center;
+}
+.mobileTopBarContent{
+    width:100%;
+}
+.DesktopTopBarContent{
+    width:70%;
+}
+.logoImageMobile{
+    width:100%;
+}
+.headerClass {
+    font-size: 16px;
+    font-weight: 600;
+    padding: 12px 0% 0% 0%;
+    display: flex;
+    flex-direction: column;
+}
+.commonHeaderClass {
+    display:flex;
+    flex-direction : row;
+    justify-content: space-evenly;
+}
+
+.emailandPhone {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 800;
+}
+
+.avatarClass {
+    display:flex;flex-direction: row; justify-content:center;
+}
+
+.mobileMenuClass{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.topBarRight {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.topBarProfile {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.iconWrap {
+    display: flex;
+    flex-direction: row;
+}
+
+.topBarRight {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.profilePhoto {
+    margin-right: 16px;
+}
+
+.iconStyle {
+    width: 100%;
+    margin-top: 8px;
+}
+
+.topBarIcon {
+    height: auto;
+    margin-right: 36px;
+}
+
+.text {
+    padding: 16px 0;
+}
+
+.profileName {
+    margin-top: 8px;
+    margin-left: 8px;
+}
+
+.dropDown {
+    margin-right: 10px;
+}
+
+.cardDesign {
+    background: #ffffff;
+    border-radius: 8px;
+}
+
+.langcardDesign {
+    height: auto;
+    width: 180px;
+    background: #ffffff;
+    border: 1px solid #d5d5d5;
+    border-radius: 8px;
+}
+
+.notcardDesign {
+    /* margin-top: -50px; */
+    width: 414px;
+    height: auto;
+    /* margin-left: 1330px; */
+    background: #ffffff;
+    border: 1px solid #d5d5d5;
+    border-radius: 8px;
+}
+
+.dropDownInfo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    /* padding: 10px; */
+    padding-top: 8px;
+    cursor: pointer;
+}
+
+.dropDownInfonot {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
--- pawntrust/src/views/JoinUs.vue
+++ pawntrust/src/views/JoinUs.vue
@@ -13,9 +13,9 @@
                             <v-text-field :rules="validateEmail" v-model="emailId" flat solo outlined placeholder="Enter Email"></v-text-field>
                             <v-btn
                             style="background-color:#F19B14;text-transform:capitalize;color:#FFF; width:100%;font-size:1rem;font-weight: 600;margin-bottom:40px"
-                            @click="Login()"
+                            @click="Submit()"
                             :disabled="(!emailId || !name || emailId == null || name == null || !isValidateEmail(emailId))"
-                            :loading="loginLoader"
+                            :loading="loader"
                             >
                                 Submit
                             </v-btn>
@@ -24,27 +24,60 @@
                 </div>
             </v-textfield>
         </div>
-        <Footer></Footer>
+        <div>
+            <v-dialog
+                v-model="dialog"
+                max-width="500"
+                persistent
+                >
+                <v-card height="200" style="display: flex;justify-content: center;align-items: center;text-align: left;padding: 0px 32px;"><span>Thank you for registering with Pawntrust Marketplace. Will be sending your pawnshop hyperlink soon to set up your profile. Thank you</span></v-card>
+                <v-btn @click="visit()" color="#F19B14" dense solo flat><span style="color: #FFF;text-transform: capitalize;">Visit our site</span></v-btn>
+            </v-dialog>
+        </div>
+        <Footer style="margin-top: 42px;"></Footer>
     </div>
 </template>
 <script>
-import TopBar from '../components/TopBar.vue';
+import TopBar from '../components/TopBarDummy.vue';
 import Footer from '../components/Footer.vue';
+import axios from 'axios';
     export default {
         components : { TopBar, Footer},
         data: () => ({
             name: '',
             emailId: '',
+            dialog: false,
+            loader: false,
             validateEmail: [ 
                     v => !!v || 'Email is required',
                     v => (/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(v)) || 'Enter valid Email'
-                    ],
+                ],
         }),
         methods: {
             isValidateEmail(v){
                 return (/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(v));
+            },
+            visit(){
+                window.open('https://pawntrust.com/');
+            },
+            Submit(){
+                let params = {
+                    "name" : this.name,
+                    "email" : this.emailId
+                }
+                axios.post('https://api.pawntrust.com/api/v1/interest', params)
+                .then(response => {
+                    console.log('response', response);
+                    this.loader = false;
+                    this.dialog = true;
+                })
+                .catch(error => {
+                    console.log('error', error);
+                    this.loader = false;
+                    this.dialog = true;
+                })
             }
-        }
+        },
     }
 </script>
 <style>
